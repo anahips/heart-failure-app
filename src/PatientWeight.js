1a407accfd4401d12eafa9ae3459add65212a348
anahips <102506256+anahips@users.noreply.github.com>
2022-07-30 14:28:46 +0100

    Fixed CSS PatientWeight

--- src/PatientWeight.js
+++ src/PatientWeight.js
@@ -5,13 +5,14 @@ export default function PatientWeight() {
   return (
     <div className="PatientWeight">
       <h5>Enter your weight in kilograms</h5>
-      <input type="text" className="weight" placeholder="example 95" />
-      <input type="submit" />
-      <br />
       <p>
         Note: It is easier to monitor significant weight changes in kilograms
         instead of stone.
       </p>
+      <form>
+        <input type="text" className="weight" placeholder="Example 95" />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
